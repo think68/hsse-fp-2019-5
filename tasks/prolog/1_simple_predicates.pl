1b0f1333c01f70b56c1c3692346538c03b0aa781
think68 <chen14.ch@mail.ru>
2019-10-22 04:06:14 +0800

    first prolog task 1_simple_predicates

--- tasks/prolog/1_simple_predicates.pl
+++ tasks/prolog/1_simple_predicates.pl
@@ -1,17 +1,38 @@
-% есть набор фактов вида father(person1, person2) (person1 is the father of person2)
+/*% есть набор фактов вида father(person1, person2) (person1 is the father of person2)
 % Необходимо определить набор предикатов:
 % 1. brother(X,Y)    -  определяющий являются ли аргументы братьями
 % 2. cousin(X,Y)     -  определяющий являются ли аргументы двоюродными братьями
 % 3. grandson(X,Y)   -  определяющий является ли аргумент Х внуком аргумента Y
 % 4. descendent(X,Y) -  определяющий является ли аргумент X потомком аргумента Y
-% 5. используя в качестве исходных данных следующий граф отношений
-	father(a,b).  % 1                 
-	father(a,c).  % 2
-	father(b,d).  % 3
-	father(b,e).  % 4
-	father(c,f).  % 5
-% указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
-	?- cousin(X,Y).
-	?- grandson(X,Y).
-	?- descendent(X,Y).
+% 5. используя в качестве исходных данных следующий граф отношений*/
+father(a,b).
+father(a,c).
+father(b,d).
+father(b,e).
+father(c,f).
+
+brother(X,Y) :- father(Z,X),father(Z,Y),X\=Y.
+cousin(X,Y) :- father(Z,X),father(W,Y),brother(Z,W),X\=Y.
+grandson(X,Y) :- father(Y,Z),father(Z,X).
+descendent(X,Y) :- father(Y,X).
+descendent(X,Y) :- father(Y,Z),descendent(X,Z).
+
+/*
+?- findall([X,Y],brother(X,Y),L).
+L = [[b, c], [c, b], [d, e], [e, d]].
+*/
+
+/*
+?- findall([X,Y],cousin(X,Y),L).
+L = [[d, f], [e, f], [f, d], [f, e]]. 
+*/
+
+/*
+?- findall([X,Y],grandson(X,Y),L).
+L = [[d, a], [e, a], [f, a]].
+*/
+
+/*
+?- findall([X,Y],descendent(X,Y),L).
+L = [[b, a], [c, a], [d, b], [e, b], [f, c], [d, a], [e, a], [f, a]]
+*/
