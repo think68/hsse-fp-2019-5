3d1700c368b0b88f5f31a44bd68f4682e2b554b8
think68 <56322487+think68@users.noreply.github.com>
2019-11-19 14:52:13 +0800

    the third task prolog
    the third task prolog

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,18 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+/* List add */
+append([],R,R).
+append([H|T],Z,[H|S]) :- append(T,Z,S).
+
+/* Segmentation list */
+partition(_,[],[],[]).
+partition(P,[H|T],[H|S],B) :- H=<P,partition(P,T,S,B).
+partition(P,[H|T],S,[H|B]) :- H>P,partition(P,T,S,B).
+
+/* Sort recursive */
+qsort([],[]).
+qsort([H|T],R) :-
+	partition(H,T,S,B),
+	qsort(S,SS),
+	qsort(B,BS),
+	append(SS,[H|BS],R).
